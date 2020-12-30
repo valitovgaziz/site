780e6f78eea457d9b078f4249af71b1bb4df7c23
gazizkv <valitovgaziz@gmail.com>
2020-12-30 12:59:27 +0500

    added JS file

---
+++ scripts.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    let drop_dawn = getElementById('content');
+    drop_dawn.addEventListener( "click", {
+        handleEvent: function(event) {
+          alert('Событие вызвал handleEvent!');
+        }} );
+    
+}
+    
+
