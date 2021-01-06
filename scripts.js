7da980d999a514b492098bcd4dc1c1ed357c450c
gazizkv <valitovgaziz@gmail.com>
2021-01-06 21:56:09 +0500

    bag fixed added shodow on gover read and fix specification

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 window.onload = function() {
     function falert() {
-      alert(this.getAttribute("height"));
-      this.setAttribute("height", "auto");
-      alert(this.getAttribute("height"));
+      this.style.height == "1em" ? this.style.height = "auto":this.style.height = "1em"
     }
     let contents = document.getElementsByClassName('content');
     for (var i=0; i<contents.length; i++) {
